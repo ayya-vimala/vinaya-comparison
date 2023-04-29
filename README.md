faaf8990cc502ab398967a8af508f645fd0d197a
ayya-vimala <ayya.vimala@gmail.com>
2023-04-29 14:29:41 +0200

    adding all csv output files

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# vinaya-comparison
+# Comparing known Vinaya texts from all schools
+
+The spreadsheet `vinaya_parallels` in vinaya_output is the main file in this repo. The other files in vinaya_output are the raw data which are combined in this file.
+This spreadsheet shows the raw dump of the parallels of the Bhikkhu and Bhikkhunī Pāṭimokkhas in all the Buddhist schools, using an API call to the SuttaCentral server for each rule.
+
+## Abbreviations
+Some explanation is needed of what you are actually looking at so let’s start by the abbreviations.
+
+Note that for some schools and texts we do not have the data. That does not mean they don’t exist, only that our data is incomplete. 
+Like for instance the Bhikkhunī Pāṭimokkha of the Mahāsaṁghika school in Sanskrit. We do however have the Chinese version.
+Also the parallels with the Khandhakas are not always complete.
+
+*Tabnames and filenames are formatted as follows: language - school - bhikkhunī or bhikkhu - textname - rule class – number*
+These numbers correspond with the actual files in SuttaCentral **
+
+### Languages
+pli  Pali
+lzh  Chinese
+san  Sanskrit
+pgd  Prakrit
+xct  Tibetan
+
+### Schools *	
+tv    Theravāda
+mg    Mahāsaṁghika
+lo    Lokuttaravāda
+mi    Mahīśāsaka
+dg    Dharmaguptaka
+sarv  Sarvāstivāda
+mu    Mūlasarvāstivāda
+
+* Next to these schools a few texts from the Bhikkhu Pāṭimokkha of other schools are mentioned like Kāśyapīya, Upāliparipṛcchā, Qizil and an other unknown school.
+In some cases some extra information is added on a version of a text if multiple versions exist, like for instance ‘tf44’ or ‘finot’.
+
+### Bhikkhunī or Bhikkhu	
+bi	Bhikkhunī
+bu	Bhikkhu
+
+### Textname	
+pm	Pāṭimokkha
+vb	Vibhaṅga
+kd	Khandhaka
+
+### Rule class	
+pj	Pārājika
+ss	Saṅghādisesa
+ay	Aniyata
+np	Nissaggiya Pācittiya
+pc	Pācittiya
+pd	Pāṭidesanīya
+sk	Sekhiya
+as	Adhikaraṇasamatha
+
+** For instance in the first spreadsheet is called pli-tv-bi-pm. This means the Pali Theravāda Bhikkhunī Pāṭimokkha.
+The first column mentions this and is marked as pli-tv-bi. Underneath you see in the first row: pm-pj1; vb-pj1
+This means Pāṭimokkha Pārājika 1 and Vibhaṅga Pārājika 1
+This translates as a link to SuttaCentral as: https://suttacentral.net/pli-tv-bu-pm-pj1 or https://suttacentral.net/pli-tv-bu-vb-pj1 to see the full text of the Vibhaṅga
+As the Pāṭimokkha is a list in one file, if you want to see that simply omit the -pj1 from the link to make https://suttacentral.net/pli-tv-bu-pm
+
+## Organisation within tabs
+
+The various tabs in the spreadsheet each take a base Pāṭimokkha which is then compared with the other texts and parallels in the canon. This base Pāṭimokkha is shown in the first column.
+Not all Pāṭimokkhas have a tab, only the major ones for which we have a larger amount of data available. Pāṭimokkha fragments of the various schools do not have their own tab for comparison.
+
+## Other files
+
+The files in the folder 'vinaya' are listings of the various texts and rules that we have available.
+The spreadsheet `extra_parallels` are parallels I added by hand as they were not in the SuttaCentral data.
---
+++ vinaya.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+""" 
+
+"""
+import os
+import re
+import json
+import requests
+import csv
+
+base_dir = os.environ['HOME']+'/vinaya-comparison/vinaya/'
+base_file = 'xct-mu-bu-pm'
+output_csv_file = os.environ['HOME']+'/vinaya-comparison/vinaya_output/'+base_file+'.csv'
+
+sortorder=['pli-tv-bi','pli-tv-bu','pli-tv-kd','lzh-mg-bi','lzh-mg-bu','san-mg-bi','san-mg-bu','san-lo-bi','san-lo-bu','lzh-mi-bi','lzh-mi-bu','lzh-dg-bi','lzh-dg-bu','pgd-dg-bi','pgd-dg-bu','lzh-sarv-bi','lzh-sarv-bu','san-sarv-bi','san-sarv-bu','san-sarv-sm','lzh-mu-bi','lzh-mu-bu','san-mu-bi','san-mu-bu','san-mu-kd','xct-mu-bi','xct-mu-bu','lzh-ka-bu','lzh-upp-bu','san-bu-pm','pgd-pm-bf13']
+
+filelist = json.loads(open(base_dir+base_file+'-tree.json', 'r', encoding='utf8').read())
+
+CsvOut = open(output_csv_file, 'w', newline='')
+parallelwriter = csv.writer(CsvOut, delimiter=',')
+parallelwriter.writerow(['Theravāda','Theravāda','Theravāda','Mahāsaṁghika','Mahāsaṁghika','Mahāsaṁghika','Mahāsaṁghika','Lokuttaravāda','Lokuttaravāda','Mahīśāsaka','Mahīśāsaka','Dharmaguptaka','Dharmaguptaka','Dharmaguptaka','Dharmaguptaka','Sarvāstivāda','Sarvāstivāda','Sarvāstivāda','Sarvāstivāda','Sarvāstivāda','Mūlasarvāstivāda','Mūlasarvāstivāda','Mūlasarvāstivāda','Mūlasarvāstivāda','Mūlasarvāstivāda','Mūlasarvāstivāda','Mūlasarvāstivāda','Kāśyapīya','Upāliparipṛcchā','Qizil','Bhikkhu Pātimokkha (unknown school)'])
+parallelwriter.writerow(['Pali','Pali','Pali','Chinese','Chinese','Sanskrit','Sanskrit','Sanskrit','Sanskrit','Chinese','Chinese','Chinese','Chinese','Prakrit','Prakrit','Chinese','Chinese','Sanskrit','Sanskrit','Sanskrit','Chinese','Chinese','Sanskrit','Sanskrit','Sanskrit','Tibetan','Tibetan','Chinese','Sanskrit','Sanskrit','Prakrit'])
+parallelwriter.writerow(['Bhikkhunī','Bhikkhu','Kandhaka','Bhikkhunī','Bhikkhu','Bhikkhunī','Bhikkhu','Bhikkhunī','Bhikkhu','Bhikkhunī','Bhikkhu','Bhikkhunī','Bhikkhu','Bhikkhunī','Bhikkhu','Bhikkhunī','Bhikkhu','Bhikkhunī','Bhikkhu','Śikṣamānā','Bhikkhunī','Bhikkhu','Bhikkhunī','Bhikkhu','Kandhaka','Bhikkhunī','Bhikkhu','Bhikkhu','Bhikkhu','Bhikkhu','Bhikkhu'])
+parallelwriter.writerow(sortorder)
+parallelwriter.writerow(['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','',''])
+temp_list = []
+
+def sortlist(parallelentrylist):
+	newlist = ['','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','']
+	for item in parallelentrylist:
+		if item.startswith('san-mu-mpt-bu'):
+			sortcode = 'san-mu-bu'
+		elif item.startswith('pli-tv-kd'):
+			sortcode = 'pli-tv-kd'
+		elif item.startswith('san-sarv-sm'):
+			sortcode = 'san-sarv-sm'
+		else:
+			sortcode = "-".join(item.split("-", 3)[:3])
+
+		if sortcode not in sortorder:
+			print(item)
+		else:
+			if item.startswith('san-mu-mpt-bu'):
+				parallelcode = re.sub('san-mu-mpt-bu-pm','pm-mpt',item)
+			else:
+				parallelcode = re.sub(sortcode,'',item).strip('-')
+			if newlist[sortorder.index(sortcode)]:
+				newlist[sortorder.index(sortcode)] += "; "+parallelcode
+			else:
+				newlist[sortorder.index(sortcode)] = parallelcode
+
+	return(newlist)
+
+for categories in filelist[base_file]:
+	for category in categories.values():
+		for file in category:
+			# print("https://suttacentral.net/api/parallels/"+file)
+			response = requests.get("https://suttacentral.net/api/parallels/"+file)
+			parallellist = response.json()[file]
+			parallelentrylist = [file]
+			for parallel in parallellist:
+				if '-' in parallel['to']['to'] and not parallel['resembling']:
+					parallelentrylist.append(parallel['to']['to'])
+				elif '-' in parallel['to']['to'] and parallel['resembling']:
+					parallelentrylist.append(parallel['to']['to']+'~')
+			
+			parallelwriter.writerow(sortlist(parallelentrylist))
+
+CsvOut.close()
+
+
